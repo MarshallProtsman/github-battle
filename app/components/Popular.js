7f680ccede0c61065674a9e8fce1f2625f90de40
marshallprotsman <47481879+MarshallProtsman@users.noreply.github.com>
2020-01-08 18:02:45 -0500

    cacheing previosly loaded repos

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,60 +1,114 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import { fetchPopularRepos } from "../utils/api";
 
 function LanguagesNav({ selected, onUpdateLanguage }) {
-    const languages = ["All", "Java", "Python", "Ruby", "JavaScript", "CSS", "GoLang", "PHP", "Dart"];
+  const languages = [
+    "All",
+    "Java",
+    "Python",
+    "Ruby",
+    "JavaScript",
+    "CSS",
+    "GoLang",
+    "PHP",
+    "Dart"
+  ];
 
-    return (
-      <ul className="flex-center">
-        {languages.map(language => (
-          <li key={language}>
-            <button
-              className="btn-clear nav-link"
-              style = {language === selected ? {color: '#455B71'} : {color: '#28343D'}}
-              onClick={() => onUpdateLanguage(language)}
-            >
-              {language}
-            </button>
-          </li>
-         ))}
-      </ul>
-    );
+  return (
+    <ul className="flex-center">
+      {languages.map(language => (
+        <li key={language}>
+          <button
+            className="btn-clear nav-link"
+            style={
+              //if the langueage is selected
+              language === selected
+                ? //be this color
+                  { color: "#455B71" }
+                : // if not, be this color
+                  { color: "#28343D" }
+            }
+            onClick={() => onUpdateLanguage(language)}
+          >
+            {language}
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
+//-------------- PROPTYPES! --------------//
 LanguagesNav.propTypes = {
   selected: PropTypes.string.isRequired,
   onUpdateLanguage: PropTypes.func.isRequired
-}
+};
 
 class Popular extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      selectedLanguage: "All"
+      selectedLanguage: "All",
+      repos: {},
+      error: null
     };
     this.updateLanguage = this.updateLanguage.bind(this);
+    this.isLoading = this.isLoading.bind(this);
   }
 
   updateLanguage(selectedLanguage) {
     this.setState({
-      selectedLanguage
+      selectedLanguage,
+      //can derive if still loading from this
+      error: null
     });
-    console.log(`You have selected ${selectedLanguage}`)
-    // alert("Pay the Troll Toll");
+
+    if (!this.state.repos[selectedLanguage]) {
+      fetchPopularRepos(selectedLanguage)
+        .then(data => {
+          this.setState(({ repos }) => ({
+            repos: {
+              ...repos,
+              [selectedLanguage]: data
+            }
+          }));
+        })
+        .catch(() => {
+          console.warn("error fetching repos D:");
+
+          this.setState({
+            error: "there was an error"
+          });
+        });
+    }
+    console.log(this.state)
   }
 
+
+  isLoading() {
+    const { selectedLanguage, repos, error } = this.state;
+    return !repos[selectedLanguage] && error === null;
+  }
   render() {
-    const { selectedLanguage } = this.state
+    const { selectedLanguage, repos, error } = this.state;
 
     return (
-        <>
-        <LanguagesNav 
-        selected = {selectedLanguage}
-        onUpdateLanguage = {this.updateLanguage}
+      <>
+        <LanguagesNav
+          selected={selectedLanguage}
+          onUpdateLanguage={this.updateLanguage}
         />
-        </>
-    )
+
+        {/* "logical operator.." if the left of the "&&" is true, the right side evaluates*/}
+        {this.isLoading() && <p>LOADING</p>}
+
+        {error && <p>{error}</p>}
+
+        {repos[selectedLanguage] && <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>}
+      </>
+    );
   }
 }
 
